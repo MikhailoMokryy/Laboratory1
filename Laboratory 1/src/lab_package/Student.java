db954347792bb02e60eb1dbdc387f26e74726236
MaxRybka <maximrmk@gmail.com>
2018-03-07 16:32:28 +0200

    input file

---
+++ Laboratory 1/src/lab_package/Student.java
@@ -0,0 +1,26 @@
+package lab_package;
+
+public class Student {
+	private String name, specialty;
+	private int course, group;
+	private Faculty faculty = new Faculty();
+
+	Student() {
+
+	}
+
+	public Student(String name, Faculty faculty, String specialty, int course, int group) {
+		this.name = name;
+
+		// for(int i=0;i<) {
+		//
+		// }
+
+		this.faculty = faculty;
+		// this.specialty = specialty.sName;
+		this.course = course;
+		this.group = group;
+		// specialty.addStudentsToSpecialty(this);
+	}
+
+}
